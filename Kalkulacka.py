c09ed05f0f92d9b59af8e05dc495f4833f9d0847
JustDave09 <david.traj@seznam.cz>
2022-03-07 21:06:33 +0100

    Initial commit XYDDDD

---
+++ Kalkulacka.py
@@ -0,0 +1,138 @@
+# Vyytvoř kalkulačku  +- */ s tkinter
+import tkinter as tk
+from functools import partial
+
+
+window = tk.Tk()
+window.geometry("1000x200")
+window.title("Kalkulačka")
+
+entry1 = tk.Entry(
+    width=100,
+    background="white",
+    foreground="black"
+)
+entry1.grid(row=0, column=1, columnspan=4)
+
+a = 0
+r = 1
+c = 1
+num = ""
+computer = ""
+
+def calc(a):
+    global num
+    num = num + str(a)
+    entry1.delete(0, tk.END)
+    entry1.insert(0, num)
+
+def answer():
+    global num
+    answer = eval(num)
+    print(answer)
+    entry1.delete(0, tk.END)
+    entry1.insert(0, answer)
+
+def reset():
+    global num
+    num = ""
+    entry1.delete(0, tk.END)
+
+while a in range(0,9):
+    if a == 0:
+        calca = partial(calc, "+")
+        print("jj")
+        buttona = tk.Button(
+            text="+",
+            bg="white",
+            fg="black",
+            width=25,
+            command=calca,
+        )
+        buttona.grid(row=1, column=4)
+
+    a += 1
+    calca = partial(calc, a)
+
+    buttona = tk.Button(
+        text=a,
+        bg="white",
+        fg="black",
+        width=25,
+        command=calca,
+    )
+    buttona.grid(row=r, column=c)
+    if a >= 3:
+        r=2
+    if a >= 6:
+        r=3
+    if a == 3:
+        calca = partial(calc, "-")
+        buttona = tk.Button(
+            text="-",
+            bg="white",
+            fg="black",
+            width=25,
+            command=calca,
+        )
+        buttona.grid(row=r, column=c+1)
+        c=0
+    elif a == 6:
+        calca = partial(calc, "*")
+        buttona = tk.Button(
+            text="*",
+            bg="white",
+            fg="black",
+            width=25,
+            command=calca,
+        )
+        buttona.grid(row=r, column=c+1)
+        c=0
+    c += 1
+
+a=0
+calca = partial(calc, a)
+
+button0 = tk.Button(
+    text="0",
+    bg="white",
+    fg="black",
+    width=25,
+    command=calca,
+)
+button0.grid(row=4, column=1)
+
+buttonReady = tk.Button(
+    text="Vypocitat",
+    bg="white",
+    fg="black",
+    width=25,
+    command=answer,
+)
+buttonReady.grid(row=4, column=2)
+
+buttonDelete = tk.Button(
+    text="Vymazat",
+    bg="white",
+    fg="black",
+    width=25,
+    command=reset,
+)
+buttonDelete.grid(row=4, column=3)
+
+calca = partial(calc, "/")
+buttonDivide = tk.Button(
+    text="/",
+    bg="white",
+    fg="black",
+    width=25,
+    command=calca,
+)
+buttonDivide.grid(row=4, column=4)
+
+
+window.mainloop()
+
+
+
+

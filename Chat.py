91e478947e91fbd00403af513f7974da74c019af
trajda20 <david.traj@seznam.cz>
2022-03-29 15:49:58 +0200

    ez pz5

--- Chat.py
+++ Chat.py
@@ -1,15 +1,17 @@
 import tkinter as tk
+import sys
 import socket
 import threading
 import errno
 
-HEADER_LENGTH = 10
+HEADER_LENGTH = 1024
+#???????????? no.0
 
 IP = "127.0.0.1"
 PORT = 1234
 
 #nastavit jméno => Grafické rozhrani pozdeji
-my_username = input("Username: ")
+user_name = input("Username: ")
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #???????????? no.1
@@ -20,83 +22,92 @@ client_socket.connect((IP, PORT))
 client_socket.setblocking(False)
 #???????????? no.1.5
 
-username = my_username.encode('utf-8')
+username = user_name.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 # Získej a odešli data na server
 
-while True:
-    #--------------------------------------------- Message input
-    message = input(f'{my_username} > ')
+def send_message():
+    message = f'{your_chat_message.get()}'
+    entry1.delete(0, tk.END)
 
     if message:
         message = message.encode('utf-8')
         message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(message_header + message)
 
-    try:
-        #--------------------------------------------- Vypiš zprávy
-        while True:
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print('Connection closed by the server')
-                # --------------------------------------------- Předělat na logging
+def get_message():
+    global HEADER_LENGTH
+    while True:
+        try:
+            #--------------------------------------------- Vypiš zprávy
+            while True:
+                username_header = client_socket.recv(HEADER_LENGTH)
+                if not len(username_header):
+                    print('Connection closed by the server')
+                    # --------------------------------------------- Předělat na logging
+                    sys.exit()
+
+                username_length = int(username_header.decode('utf-8').strip())
+                username = client_socket.recv(username_length).decode('utf-8')
+
+                message_header = client_socket.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode('utf-8').strip())
+                message = client_socket.recv(message_length).decode('utf-8')
+
+                #--------------------------------------------- Vypiš zprávy
+                label1.insert(0, f'{username} > {message}')
+
+
+        except IOError as e:
+            # --------------------------------------------- Předělat na logging -------------------------------------------
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print('Reading error: {}'.format(str(e)))
                 sys.exit()
 
-            username_length = int(username_header.decode('utf-8').strip())
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            #--------------------------------------------- Vypiš zprávy
-            print(f'{username} > {message}')
-
-    except IOError as e:
-        # --------------------------------------------- Předělat na logging -------------------------------------------
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
+        except Exception as e:
+            print('Reading error: '.format(str(e)))
             sys.exit()
-        continue
-
-    except Exception as e:
-        print('Reading error: '.format(str(e)))
-        sys.exit()
 
 
 #Tady začíná Grafické rozhraní chatu
 
 window = tk.Tk()
-window.geometry("750x720")
+window.geometry("580x220")
 window.title("Chat")
+window.configure(bg="black")
 
-label1 = tk.Label(
-    text=message,
+label1 = tk.Listbox(
     background="black",
-    foreground="white",
-    font=("Arial", 20),
+    foreground="#7bff00",
+    width=63,
+    font=(20),
 )
 label1.grid(row=0, column=1, columnspan=4, sticky="W")
 
+your_chat_message = tk.StringVar()
 entry1 = tk.Entry(
-    width=32,
-    font=("Arial", 40),
-    background="white",
-    foreground="black"
+    textvariable=your_chat_message,
+    width=23,
+    font=("Arial", 34),
+    background="black",
+    foreground="white",
+    highlightbackground="black"
 )
-entry1.grid(row=1, column=1, columnspan=4)
+entry1.grid(row=1, column=0, columnspan=4)
 
 button1 = tk.Button(
-    font=("Arial", 40),
+    window,
+    font=("Arial", 20),
     text="Odeslat",
     bg="white",
-    fg="black",
-    width=33,
-    height=1,
+    highlightbackground="white",
+    foreground="black",
+    width=10,
+    height=2,
     command=send_message,
 )
-button1.grid(row=2, column=1)
+button1.grid(row=1, column=4)
 
 x = threading.Thread(target=get_message)
 x.start()
--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 import socket
 import select
+import threading
+import logging
 
-HEADER_LENGTH = 10
+logging.basicConfig(filename='server.log', level=logging.DEBUG, filemode='a+', format='[%(asctime)s] %(levelname)s %(threadName)s %(name)s: %(message)s')
+logging.info('-----------------------------------------------------------------')
+logging.info('Starting server "The_Best_chatting_service_2022.jar"')
+logging.info('-----------------------------------------------------------------')
+
+
+HEADER_LENGTH = 1024
 
 IP = "127.0.0.1"
 PORT = 1234
 
+logging.info('Creating server at ' + str(IP) + " on port" + str(PORT))
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.AF_INET - rodina sítě
@@ -17,6 +26,7 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((IP, PORT))
 # nabindovat server na socket který bude používat
 # 0.0.0.0 oddposlouchává veškeré použitelné síte! :o
+logging.info('Succesfully bound to socket')
 
 server_socket.listen()
 # server čeké na nové připojení
@@ -26,78 +36,82 @@ sockets_list = [server_socket]
 
 clients = {}
 
-print(f'Listening for connections on {IP}:{PORT}...')
+logging.info('Listening for connections on ' + str(IP) + ":" + str(PORT))
 #-----------------------------------------------------------Předělat na logging
 
 # Následující funkce spravuje příchozí zprávy
 def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-        # ???????????? no.3
+    while True:
+        try:
+            message_header = client_socket.recv(HEADER_LENGTH)
+            # ???????????? no.3
 
-        # Pokud nepřišla zpráva:
-        if not len(message_header):
+            # Pokud nepřišla zpráva:
+            if not len(message_header):
+                return False
+
+            message_length = int(message_header.decode('utf-8').strip())
+            # ???????????? no.4
+
+            return {'header': message_header, 'data': client_socket.recv(message_length)}
+            # ???????????? Vrátí zprávu? no.4.5
+        except:
+            # Pokud skončilo připojení. . . Logging?
             return False
 
-        message_length = int(message_header.decode('utf-8').strip())
-        # ???????????? no.4
+def server():
+    while True:
+        read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+        # ???????????? no.5
 
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-        # ???????????? Vrátí zprávu? no.4.5
-    except:
-        # Pokud skončilo připojení. . . Logging?
-        return False
+        # Prohlíží příchozí připojení
+        for notified_socket in read_sockets:
 
-while True:
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
-    # ???????????? no.5
+            # Pokud se jedná o nové připojení => Přijmi
+            if notified_socket == server_socket:
+                client_socket, client_address = server_socket.accept()
+                # ???????????? no.6
 
-    # Prohlíží příchozí připojení
-    for notified_socket in read_sockets:
+                user = receive_message(client_socket)
+                #Získej jméno uživatele (první zpráva)
 
-        # Pokud se jedná o nové připojení => Přijmi
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            # ???????????? no.6
+                # Pokud se odpojí před odesláním jména:
+                if user is False:
+                    continue
 
-            user = receive_message(client_socket)
-            #Získej jméno uživatele (první zpráva)
+                sockets_list.append(client_socket)
+                # ???????????? no.7
+                clients[client_socket] = user
 
-            # Pokud se odpojí před odesláním jména:
-            if user is False:
-                continue
-
-            sockets_list.append(client_socket)
-            # ???????????? no.7
-            clients[client_socket] = user
-
-            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
-            # -----------------------------------------------------------Předělat na logging
-        # Pokud posílá zprávu:
-        else:
-            # Zpráva
-            message = receive_message(notified_socket)
-
-            # Pokud se odpojí:
-            if message is False:
-                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                logging.info('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
                 # -----------------------------------------------------------Předělat na logging
-                # Vymaž z listu připojených uživatelů
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
+            # Pokud posílá zprávu:
+            else:
+                # Zpráva
+                message = receive_message(notified_socket)
 
-            # Zjisti kdo píše zprávu:
-            user = clients[notified_socket]
+                # Pokud se odpojí:
+                if message is False:
+                    logging.info('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+                    # -----------------------------------------------------------Předělat na logging
+                    # Vymaž z listu připojených uživatelů
+                    sockets_list.remove(notified_socket)
+                    del clients[notified_socket]
+                    continue
 
-            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
-            # -----------------------------------------------------------Předělat na logging
-            # Odešli zprávu všem připojeným:
-            for client_socket in clients:
-                # Pokud se nejedná o odesílatele
-                if client_socket != notified_socket:
+                # Zjisti kdo píše zprávu:
+                user = clients[notified_socket]
+
+                logging.info(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+                # -----------------------------------------------------------Předělat na logging
+                # Odešli zprávu všem připojeným:
+                for client_socket in clients:
+                    # Pokud se nejedná o odesílatele
+                    #if client_socket != notified_socket:
                     client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
+x2 = threading.Thread(target=server)
+x2.start()
 """
     #Příprava na pokročilejí logging
     for notified_socket in exception_sockets:
